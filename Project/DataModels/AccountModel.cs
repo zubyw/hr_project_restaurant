5113dc7e5018646a84cfc1c091be9d4c505342c0
Zuby <zubyw2012@gmail.com>
2025-09-22 15:17:13 +0200

    first commit

---
+++ Project/DataModels/AccountModel.cs
@@ -0,0 +1,23 @@
+﻿public class AccountModel
+{
+
+    public Int64 Id { get; set; }
+    public string EmailAddress { get; set; }
+
+    public string Password { get; set; }
+
+    public string FullName { get; set; }
+
+    public AccountModel(Int64 id, string email, string password, string fullname)
+    {
+        Id = id;
+        EmailAddress = email;
+        Password = password;
+        FullName = fullname;
+    }
+
+
+}
+
+
+
