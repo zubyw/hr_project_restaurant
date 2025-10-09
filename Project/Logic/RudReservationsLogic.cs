f2fcd6f4d337e60748a02ed464519452abf54816
Kavishh07 <kavish0712@outlook.com>
2025-10-09 12:32:59 +0200

    Logic layer update & delete reservation

--- Project/Logic/RudReservationsLogic.cs
+++ Project/Logic/RudReservationsLogic.cs
@@ -16,6 +16,21 @@ namespace Project.Logic
             return access.GetReservationsByUserId(userId);
         }
 
+        // method that updates reservation
+        public void UpdateReservation(int id, int guestCount, string startAt)
+        {
+            if (guestCount <= 0 || string.IsNullOrEmpty(startAt))
+            return;
+
+            access.UpdateReservation(id, guestCount, startAt);
+        }
+
+        // method that deletes reservation
+        public void DeleteReservation(int id)
+        {
+            access.DeleteReservation(id);
+        }
     }
 }
+
 
