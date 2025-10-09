52cf37adc841107f0a83d033a4699bd50758cfea
Kavishh07 <kavish0712@outlook.com>
2025-10-09 12:24:51 +0200

    Logic layer reservation DATA

---
+++ Project/Logic/RudReservationsLogic.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Project.DataAccess;
+using Project.DataModels;
+
+namespace Project.Logic
+{
+    // logic class that handles the reservation
+    public class RudReservationsLogic
+    {
+        // instance DataAccess
+        RudReservationsAccess access = new RudReservationsAccess();
+
+        // method that gets all reservations with userId
+        public List<ReservationModel> GetReservations(int userId)
+        {
+            return access.GetReservationsByUserId(userId);
+        }
+
+    }
+}
+
