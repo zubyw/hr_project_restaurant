2d2541d35fba49574b74fb7f5411adf382726f85
Daan Koster <daanhendrikkoster@gmail.com>
2025-10-29 16:26:52 +0100

    Made ThemeModel
    Made ThemeModel

---
+++ Project/DataModels/ThemeModel.cs
@@ -0,0 +1,9 @@
+public class ThemeModel
+{
+
+    public int ID { get; set; }
+    public required string Name { get; set; }
+    public required decimal Course { get; set; }
+    public required string IsActive { get; set; }
+
+}
