fc1a84d975b57a01ea76f77b0414cf2eba2c7f01
Kavishh07 <kavish0712@outlook.com>
2025-12-16 13:39:05 +0100

    DrinkModel.cs

---
+++ Project/DataModels/DrinkModel.cs
@@ -0,0 +1,15 @@
+public class Drink
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double AlcoholPercentage { get; set; }
+    public decimal Price { get; set; }
+
+    public Drink(int id, string name, double alcoholPercentage, decimal price)
+    {
+        Id = id;
+        Name = name;
+        AlcoholPercentage = alcoholPercentage;
+        Price = price;
+    }
+}
