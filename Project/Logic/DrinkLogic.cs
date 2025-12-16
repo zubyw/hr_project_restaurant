d243e58166cbcd7242734de6786c5af05ddb50cb
Kavishh07 <kavish0712@outlook.com>
2025-12-16 14:12:37 +0100

    Access /  Create method for DrinkLogic

---
+++ Project/Logic/DrinkLogic.cs
@@ -0,0 +1,17 @@
+using Project.DataAccess;
+using Project.DataModels;
+using System.Collections.Generic;
+
+namespace Project.Logic
+{
+    public class DrinkLogic
+    {
+        private DrinkAccess _drinkAccess = new DrinkAccess();
+
+        public void CreateDrink(string name, double alcoholPercentage, decimal price)
+        {
+            Drink drink = new Drink(0, name, alcoholPercentage, price);
+            _drinkAccess.Write(drink);
+        }
+    }
+}
